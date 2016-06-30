38ab7dbca0240559dc6f443d37cebceb49a0b383
Leonardo <c123davidleo@gmail.com>
2016-06-30 01:35:12 -0500

    asd

--- sockets/src/logic/Runner.java
+++ sockets/src/logic/Runner.java
@@ -4,11 +4,9 @@ public class Runner {
 
 	public static void main(String[] args){
 		
-		boolean serverData;
-		
 		
 		Node node = new Node(Integer.parseInt(args[0]), args[1], Integer.parseInt(args[2]), args[3].equals("true"), args[4].equals("true"));
-		Node node2 = new Node(1234, "localhost", 1212, false, false);
+		//Node node2 = new Node(1234, "localhost", 1212, false, false);
 		
 	}
 }
